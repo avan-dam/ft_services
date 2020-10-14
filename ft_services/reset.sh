e3631fb875cf646bb87e0009c640010a164489d0
Amber Van dam <avan-dam@f1r6s9.codam.nl>
2020-10-14 17:33:29 +0200

    think me nginx is working with the image now

---
+++ ft_services/reset.sh
@@ -0,0 +1,5 @@
+minikube stop 
+
+minikube delete
+
+bash setup.sh
--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -3,24 +3,24 @@ mkdir -p ~/goinfre/.minikube
 ln -s ~/goinfre/.minikube ~/.minikube
 
 minikube start	--vm-driver=virtualbox  \
-				--cpus=2 --memory 1500 \
-                --no-vtx-check
+				--cpus=2 --memory 2000 \
+                --no-vtx-check \
+                # check what above does
+                # --addons metalldb \
+                --addons dashboard
 
-# kubernetes deployment using existing image named echoserver and exposed it on port 8080 using --port
-kubectl create deployment hello-minikube --image=k8s.gcr.io/echoserver:1.10
+# minikube addons enable metallb
 
-# install MetalLB by manifest
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 # MetalLB remains idle until configured. This is accomplished by creating and deploying a configmap
-kubectl apply -f config.yaml
+kubectl apply -f srcs/metallb.yaml
 
-# Create a Deployment based on the of NGINX based on generic YAML file:
-# https://kubernetes.io/docs/tasks/run-application/run-stateless-application-deployment/
-kubectl apply -f https://k8s.io/examples/application/deployment.yaml
+# Create a Deployment based on the of NGINX based on generic YAML file: (https://kubernetes.io/docs/tasks/run-application/run-stateless-application-deployment/)
+kubectl apply -f srcs/nginx/deployment.yaml
 
 
 # printf "Building and deploying nginx:\t\t"
