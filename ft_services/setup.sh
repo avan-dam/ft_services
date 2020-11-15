559c13380b14ca3ddf6fe6c4150488bacb3f3097
Amber Van dam <avan-dam@f1r6s13.codam.nl>
2020-11-15 15:29:30 +0100

    dockerfiles for GRAFANA AND INFLUXDB AND TELEGRAF WORKING

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -15,12 +15,15 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 # MetalLB remains idle until configured. This is accomplished by creating and deploying a configmap
 kubectl apply -f srcs/metallb.yaml
+
 eval $(minikube docker-env)
 docker build -t mynginx srcs/nginx/  
 docker build -t my_mysql srcs/mysql/  
 docker build -t my_phpmyadmin srcs/phpmyadmin/  
 docker build -t my_wordpress srcs/wordpress/  
 docker build -t my_grafana srcs/grafana/  
+docker build -t my_influxdb srcs/influxdb/  
+docker build -t my_telegraf srcs/telegraf/  
 
 # Create a Deployment based on the of NGINX based on generic YAML file: (https://kubernetes.io/docs/tasks/run-application/run-stateless-application-deployment/)
 kubectl apply -f srcs/nginx/nginx.yaml
@@ -36,16 +39,7 @@ kubectl apply -f srcs/grafana/grafana.yaml
 # AND influxdb
 kubectl apply -f srcs/influxdb/influxdb.yaml 
 # AND telegraf
-kubectl apply -f srcs/influxdb/telegraf.yaml 
+kubectl apply -f srcs/telegraf/telegraf.yaml 
 # and with FTPS
 # kubectl apply -f srcs/ftps/vsftpd.conf
 # kubectl apply -f srcs/ftps/ftps.yaml
-
-# printf "Building and deploying nginx:\t\t"
-# docker build -t nginx_alpine ./srcs/nginx 
-# # > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/nginx.yaml >> log.log 2>> errlog.txt
-# # get a nginx file using alpine linux and try and get it working
-# # metalb needed to make this work
-# # docker run -d -p 80:80 nginx_alpine
-# docker ps
-# ifconfig
